21a5e29b50ca0515c9b98f64b8d0b5ccb63cddfb
pyw424 <parkyw0424@naver.com>
2023-12-05 13:45:38 +0900

    예원 커밋!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import TravelPlaceSearch from "./pages/Search";
+import TravelPlaceInfo from "./pages/Search";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<TravelPlaceSearch />} />
+      <Route path="/" element={<TravelPlaceInfo />} />
     </Routes>
   );
 }
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,35 +1,64 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react";
 
-export default function TravelPlaceSearch() {
-  const [inputValue, setInputValue] = useState("");
-  const [places] = useState([]);
+const TravelPlaceInfo = () => {
+  const [places, setPlaces] = useState([]);
+  const [error, setError] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  const searchPlaces = () => {
-    const apiUrl = `https://apis.data.go.kr/B551011/KorService1/locationBasedList1?serviceKey=pPM2VrV7r3IwxAFI87FWbmu2fU3hy8eCslZyp39YTXfpn8XgrMIRLmMYpk6Vvpic15howkhlzMKfieBlGbOhPA%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=A&mapX=126.981611&mapY=37.568477&radius=1000&contentTypeId=15`;
+  const fetchData = async () => {
+    try {
+      const response = await fetch();
+      if (!response.ok) {
+        throw new Error("데이터를 불러오는 중 문제가 발생했습니다.");
+      }
 
-    axios.get(apiUrl);
+      const data = await response.json();
+      const items = data.response.body.items.item;
+
+      setPlaces(items);
+      setError(null);
+    } catch (error) {
+      console.error("데이터 요청 중 오류 발생:", error);
+      setPlaces([]);
+      setError("데이터를 불러오는 중 문제가 발생했습니다.");
+    }
+  };
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    fetchData();
   };
 
   return (
     <div>
+      <h1>여행지 정보</h1>
       <input
         type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        placeholder="장소를 입력하세요"
+        value={searchTerm}
+        onChange={handleSearch}
+        placeholder="여행지를 검색하세요"
       />
-      <button onClick={searchPlaces}>검색</button>
-
-      <div>
-        {places.map((place, index) => (
-          <div key={index}>
-            <strong>{place.name}</strong>
-            <br />
-            {place.formatted_address}
-          </div>
-        ))}
-      </div>
+      <button onClick={handleSubmit}>찾기</button>
+      {error && <p>{error}</p>}
+      <ul>
+        {places &&
+          places.map((place, index) => (
+            <li key={index}>
+              <h2>{place.title}</h2>
+              <p>
+                주소: {place.addr1} {place.addr2}
+              </p>
+              <p>지역 코드: {place.areacode}</p>
+              <p>주소1: {place.addr1}</p>
+              <img src={place.firstimage} alt={`이미지_${index}`} />
+            </li>
+          ))}
+      </ul>
     </div>
   );
-}
+};
+
+export default TravelPlaceInfo;
